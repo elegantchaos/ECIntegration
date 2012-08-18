7932da1a31d61812c66d9c92a12c7bd08b0b8bc5
Sam Deane <sam@elegantchaos.com>
2012-08-18 19:43:54 +0100

    added tests for other libraries

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -7,5 +7,23 @@ project=ECCore
 base=`dirname $0`
 source "$base/../ECUnitTests/Resources/Scripts/test-common.sh"
 
+# ECCore tests
+
 macbuild "ECCore Mac" test
 iosbuild "ECCore iOS" test
+
+# ECLogging tests
+
+macbuild "ECLogging Mac" test
+iosbuild "ECLogging iOS" test
+
+#macbuild "ECLogging Sample Mac" build
+#iosbuild "ECLogging Sample iOS" build
+
+# ECUnitTests tests
+
+#macbuild "ECUnitTest Example Mac" test
+#iosbuild "ECUnitTest Example iOS" build
+
+#iosbuildproject "ECUnitTests" "ECUnitTestsIOS"
+#iostestproject "ECUnitTestsExample" "iOS Tests"
