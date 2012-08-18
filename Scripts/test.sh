eaa42b83e54b2997fe63f6391004c7564446865b
Sam Deane <sam@elegantchaos.com>
2012-08-18 21:05:26 +0100

    reverted to one test script, so that all unit tests are recorded

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
-# Test script for ECUnitTests
+# Test script for various EC modules
 
 project=ECIntegration
 
 base=`dirname $0`
 source "$base/../ECUnitTests/Resources/Scripts/test-common.sh"
 
+# test ECCore
+
+macbuild "ECCore Mac" test
+iosbuild "ECCore iOS" test
+
+# test ECUnitTests
+
 macbuild "ECUnitTest Example Mac" test
 iosbuild "ECUnitTest Example iOS" build
 
 iosbuildproject "ECUnitTests" "ECUnitTestsIOS"
 iostestproject "ECUnitTestsExample" "iOS Tests"
+
+# test ECLogging
+
+macbuild "ECLogging Mac" test
+iosbuild "ECLogging iOS" test
+
+macbuild "ECLogging Sample Mac" build
+iosbuild "ECLogging Sample iOS" build
