b4885d8cc899db2044935726870de247e1b566b3
Sam Deane <sam@elegantchaos.com>
2013-02-28 14:54:19 +0000

    Fixes for test script

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -5,28 +5,16 @@
 project=ECIntegration
 
 base=`dirname $0`
-source "$base/../ECUnitTests/Resources/Scripts/test-common.sh"
+source "$base/../ECLogging/Extras/Scripts/test-common.sh"
 
-# test ECCore
-
-macbuild "ECCore Mac" test
-iosbuild "ECCore iOS" test
-
-# test ECUnitTests
-
-macbuild "ECUnitTest Example Mac" test
-iosbuild "ECUnitTest Example iOS" build
-
-iosbuildproject "ECUnitTests" "ECUnitTestsIOS"
-iostestproject "ECUnitTestsExample" "iOS Tests"
 
 # test ECLogging
 
 macbuild "ECLogging Mac" test
 iosbuild "ECLogging iOS" test
 
-macbuild "ECLogging Sample Mac" build
-iosbuild "ECLogging Sample iOS" build
+macbuild "ECLogging Sample Mac" build test
+iosbuild "ECLogging Sample iOS" build test
 
 # test ECLocation
 
