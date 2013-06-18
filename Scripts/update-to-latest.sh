ba53eb6d393e3f95c8e9f8962ffa77a0830c3614
Sam Deane <sam@elegantchaos.com>
2013-06-18 22:06:47 +0100

    Added update script

---
+++ Scripts/update-to-latest.sh
@@ -0,0 +1,21 @@
+base=`dirname $0`
+pushd "$base" > /dev/null
+full="$PWD"
+popd > /dev/null
+
+STATUS=`git status -s`
+
+if [[ "$STATUS" == "" ]]; then
+
+    echo "Pulling latest submodules"
+    "$base/..ECLogging/Extras/Scripts/pull-latest-submodules.sh"
+
+    STATUS=`git status -s`
+
+    if [[ "$STATUS" != "" ]]; then
+        echo "Submodules have changed - committing"
+        git commit . -m "Automatic update of submodules"
+        git push
+
+    fi
+fi
