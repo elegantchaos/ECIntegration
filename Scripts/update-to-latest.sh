c70ccaf0f9968623cca253f700fa4178b2acfd5f
Sam Deane <sam@elegantchaos.com>
2013-06-18 22:07:59 +0100

    Added update script

--- Scripts/update-to-latest.sh
+++ Scripts/update-to-latest.sh
@@ -7,15 +7,20 @@ STATUS=`git status -s`
 
 if [[ "$STATUS" == "" ]]; then
 
-    echo "Pulling latest submodules"
-    "$base/..ECLogging/Extras/Scripts/pull-latest-submodules.sh"
+    echo "Pulling latest submodules."
+    "$base/../ECLogging/Extras/Scripts/pull-latest-submodules.sh"
 
     STATUS=`git status -s`
 
     if [[ "$STATUS" != "" ]]; then
-        echo "Submodules have changed - committing"
+        echo "Submodules have changed - committing."
         git commit . -m "Automatic update of submodules"
         git push
 
     fi
+
+else
+
+    echo "You have changes outstanding in git - commit them first."
+
 fi
