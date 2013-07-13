de068336135874911c6c09c579ea505fbedf3cae
Sam Deane <sam@elegantchaos.com>
2013-07-13 17:52:40 +0100

    updated jenkins script

--- Scripts/jenkins.sh
+++ Scripts/jenkins.sh
@@ -2,22 +2,9 @@
 
 base=`dirname $0`
 
-status=`git status -s`
-if [[ "$status" != "" ]]; then
-	echo "Git seems to have changes outstanding."
-	exit 1
-fi
+git submodule sync > /dev/null
+git submodule update --init --recursive
+git submodule foreach 'git remote set-url --push origin git@github.com:elegantchaos/$name.git'
 
-echo "Pulling latest submodules"
-ECLogging/Extras/Scripts/pull-latest-develop-submodules.sh
-
-status=`git status -s`
-if [[ "$status" != "" ]]; then
-
-	echo "Submodules updated"
-	git commit . -m "updated to latest modules"
-
-	echo "Running tests"
-	"$status/test.sh"
-	
-fi
+echo "Running tests"
+"$base/test.sh"
