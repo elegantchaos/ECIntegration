05e17a86d897fed7e7e3f6c2ecbef5d744d64af9
Sam Deane <sam@elegantchaos.com>
2013-07-13 17:03:15 +0100

    tweaked jenkins script

--- Scripts/jenkins.sh
+++ Scripts/jenkins.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-status=`git status -s`
 base=`dirname $0`
 
+status=`git status -s`
 if [[ "$status" != "" ]]; then
 	echo "Git seems to have changes outstanding."
 	exit 1
 fi
 
+echo "Pulling latest submodules"
 ECLogging/Extras/Scripts/pull-latest-submodules.sh
 
 status=`git status -s`
-
 if [[ "$status" != "" ]]; then
 	
 	git commit . -m "updated to latest modules"
