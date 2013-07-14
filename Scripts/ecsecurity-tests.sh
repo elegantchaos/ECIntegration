508208b7f170f0bfca37fb6a64330502d4c86130
Sam Deane <sam@elegantchaos.com>
2013-07-14 12:56:43 +0100

    added scripts

---
+++ Scripts/ecsecurity-tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Test script for various EC modules
+
+project=ECIntegration
+
+base=`dirname $0`
+ecbase="$base/../ECLogging/Extras/Scripts"
+source "$ecbase/test-common.sh"
+
+# test ECSecurity
+
+macbuild "ECSecurity Mac" test
+iosbuild "ECSecurity iOS" test
+
+
+
+# if everything worked, try to push the current submodule revisions to their master branches
+#git submodule foreach 'git push origin HEAD:master'
